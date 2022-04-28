65b844e16288a4bcc99ed1f138380c0b344e0b4c
a-butterman <apmyp.jezz@gmail.com>
2022-04-28 19:57:37 +0300

    1 lesson complete

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import Message from "./Message";
 
 const messageData = {
-    avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
-    name: 'Some Name',
-    message: 'some text',
-    time: '22:00',
+    avatar: "https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg",
+    name: 'Artem',
+    message: 'npm start нажимал?',
+    time: '20:00'
 }
 
-function HW1() {
+const HW1 = () => {
     return (
         <div>
             <hr/>
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import mess from './Message.module.css'
 
-function Message() {
+type MessagePropsType = {
+    avatar: any
+    name: string
+    message: string
+    time: string
+}
+
+const Message = (props: MessagePropsType) => {
     return (
-        <div>
-
+        <div className={mess.body}>
+            <div>
+                <img className={mess.avatar} src={props.avatar} alt="avatar"></img>
+            </div>
+            <div className={mess.cloud}>
+                <div className={mess.name}>{props.name}</div>
+                <div className={mess.message}>{props.message}</div>
+                <div className={mess.time}>{props.time}</div>
+            </div>
         </div>
     )
 }
