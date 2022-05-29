b7ad01d8180c402c01ec14f6a6b4192045e2dd8a
a-butterman <apmyp.jezz@gmail.com>
2022-05-30 02:39:50 +0300

    added 5 lesson, routed dom

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
+import './Header.css';
 
 function Header() {
     return (
         <div>
-            // add NavLinks
-
+            <input className='hidden__checkbox' type='checkbox' id='menu'/>
+            <span className='menu'>
+                <NavLink to={'./pre-junior'}>PRE JUNIOR</NavLink>
+                <NavLink to={'./junior'}>JUNIOR</NavLink>
+                <NavLink to={'./junior-Plus'}>JUNIOR PLUS</NavLink>
+            </span>
+            <label className='hidden__button' htmlFor='menu'>☰</label>
         </div>
     )
 }
--- src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import s from './Error404.module.css'
 
 function Error404() {
     return (
         <div>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+            <div className={s.er404}>
+                <img src="https://i.yapx.ru/SKLUI.png" alt="404"/>
+                <img src="https://i.yapx.ru/SKLUH.png" alt="wasted"/>
+            </div>
+            <img src="https://c.tenor.com/eMIEa2R-I5AAAAAd/gta-sa-gta.gif" alt="404 ERROR" width={'100%'}/>
         </div>
     )
 }
